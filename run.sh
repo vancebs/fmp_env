0e8580866d9826d677172aaa3a492430fadba317
vancebs <vancebs@qq.com>
2017-05-11 09:12:59 +0800

    update

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@
 /usr/local/bin/redis-server /usr/local/redis/conf/redis6381.conf
 /usr/local/bin/redis-server /usr/local/redis/conf/sentinel26379.conf --sentinel &
 /usr/local/bin/redis-server /usr/local/redis/conf/sentinel26479.conf --sentinel &
-/usr/local/bin/redis-server /usr/local/redis/conf/sentinel26579.conf --sentinel &
+/usr/local/bin/redis-server /usr/local/redis/conf/sentinel26579.conf --sentinel
