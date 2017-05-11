cbddbd9d99edbecdb3f441e17f85e8dc9f8cb7ad
vancebs <vancebs@qq.com>
2017-05-11 10:58:42 +0800

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 /usr/local/bin/redis-server /usr/local/redis/conf/redis6379.conf
-/usr/local/bin/redis-server /usr/local/redis/conf/redis6380.conf
-/usr/local/bin/redis-server /usr/local/redis/conf/redis6381.conf
-/usr/local/bin/redis-server /usr/local/redis/conf/sentinel26379.conf --sentinel &
-/usr/local/bin/redis-server /usr/local/redis/conf/sentinel26479.conf --sentinel &
-/usr/local/bin/redis-server /usr/local/redis/conf/sentinel26579.conf --sentinel
+#/usr/local/bin/redis-server /usr/local/redis/conf/redis6380.conf
+#/usr/local/bin/redis-server /usr/local/redis/conf/redis6381.conf
+/usr/local/bin/redis-server /usr/local/redis/conf/sentinel26379.conf --sentinel
+#/usr/local/bin/redis-server /usr/local/redis/conf/sentinel26479.conf --sentinel &
+#/usr/local/bin/redis-server /usr/local/redis/conf/sentinel26579.conf --sentinel
